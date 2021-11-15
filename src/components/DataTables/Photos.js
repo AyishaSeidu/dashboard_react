6d8a0ac03e1e0aff6c127be1711722b365d651d6
Ayisha <ayishetuseidu13@gmail.com>
2021-11-15 23:04:08 +0000

    add styling to photos table

--- src/components/DataTables/Photos.js
+++ src/components/DataTables/Photos.js
@@ -6,7 +6,7 @@ function Photos({data, urlhead}) {
     return (
         <TableContainer>
             {data.map((item)=> {
-                return <Image src={item[urlhead]} />
+                return <Image key={item?.id} src={item[urlhead]} />
             })}
         </TableContainer>
     )
@@ -15,7 +15,14 @@ function Photos({data, urlhead}) {
 export default Photos
 
 const Image = styled.img`
+display: inline;
 margin: 0.2rem;
 border-radius: 0.5rem ;
+height: 4rem;
+width: 4rem;
+/* @media (${({theme})=>theme.mediaquery.smallScreens}) {
+  height: 20%;
+  width: 30%;
+} */
 `;
 
